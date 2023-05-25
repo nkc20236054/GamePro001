1a46a31cfd2fb6ea065daac7d12d6a3ff4f7cd36
nkc20236054 <nkc23.kuzuha.mitsuki@gmail.com>
2023-05-25 12:51:54 +0900

    0525update

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,21 +8,26 @@ using UnityEngine.UI;
 public class GameDirector : MonoBehaviour
 {
     GameObject timeGauge;
+    GameObject meterText;
+    float meter = 0;
     void Start()
     {
         this.timeGauge = GameObject.Find("timeGauge");
+        this.meterText = GameObject.Find("meter");
     }
     void Update()
     {
-        this.timeGauge.GetComponent<Image>().fillAmount -= 0.00028f;
+        this.timeGauge.GetComponent<Image>().fillAmount -= 0.0001667f;
         if (this.timeGauge.GetComponent <Image>().fillAmount <= 0)
         {
             SceneManager.LoadScene("TitleScene");
         }
+        this.meter += Time.deltaTime*60;
+        this.meterText.GetComponent<Text>().text = this.meter.ToString("F1") + "km";
     }
 
     public void Decreacetime()
     {
-        this.timeGauge.GetComponent<Image>().fillAmount -= 0.155f;
+        this.timeGauge.GetComponent<Image>().fillAmount -= 0.09f;
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
     Vector3 pos;
+    Animator animator;
     void Start()
     {
         Application.targetFrameRate = 60;
+        this.animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(x * 0.3f, 0, 0);
-        float y = Input.GetAxisRaw("Vertical");
-        transform.position += new Vector3(0, y*0.3f, 0);
+        float InputX = UnityEngine.Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(InputX * 0.3f, 0);
+        float InputY = UnityEngine.Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(0, InputY*0.3f);
+        if (InputY > 0.01f)
+        { this.animator.SetTrigger("LeftTrigger"); }
+        else if (InputY< -0.01f) 
+        { this.animator.SetTrigger("RightTrigger"); }
         pos = transform.position;
         if (pos.x <= -9)
         {
